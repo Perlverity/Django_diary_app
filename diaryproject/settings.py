e25b8121553a6bcc39d9f250d97ee5e463a8a29b
Perlverity <snowman.ss.6124@gmail.com>
2023-01-03 00:25:14 +0900

    #03: fix: str

--- diaryproject/settings.py
+++ diaryproject/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'diaryproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        # 'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': ['diaryapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
